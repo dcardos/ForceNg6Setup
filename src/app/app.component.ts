aceae52eb45da6927b68d06b51d72928eee9295a
Danilo Cardoso <danilo.cardoso@stone.com.br>
2018-08-07 13:38:10 -0300

    all project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SalesforceApiService } from './sf-api-service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+    constructor(private _sfApi: SalesforceApiService) {
+
+    }
+
+    title = 'app';
+
+    ngOnInit() {
+        this._sfApi.helloAngular("fellow enthusiast").subscribe((name) => {
+            this.title = name;
+        });
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { SalesforceHashLocationStrategy } from './util/sf-path-location-strategy';
+import { LocationStrategy } from '@angular/common';
+import { StaticPathPipe } from './pipes/static-path.pipe';
+import { SalesforceApiService } from './sf-api-service';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StaticPathPipe
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+      SalesforceApiService,
+      {
+          provide: LocationStrategy,
+          useClass: SalesforceHashLocationStrategy
+      }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
